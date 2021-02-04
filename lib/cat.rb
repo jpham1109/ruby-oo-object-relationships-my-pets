3efbe44538db0d88f6bf6c2f2bd421b01c1b7bc0
Jamie H Pham <jpham1109@gmail.com>
2021-02-04 16:37:29 -0500

    commit tree 1dc2fadf5d8e8ae092ee01f4bf2873438c20ef62

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all 
+  end
+  
+  def self.count
+    @@all.size
+  end
 end
+
+
+#binding.pry
+0
---
+++ lib/run.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require_relative 'owner'
+require_relative 'dog'
+require_relative 'cat'
+
+justine = Owner.new("Justine")
+
+
